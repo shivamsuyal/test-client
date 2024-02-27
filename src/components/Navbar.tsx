97b352d98dd88782d784659e598f1b89aa00e62f
shivamsuyal <shivamsuyal25@gmail.com>
2024-02-27 11:45:21 +0530

    commit

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,6 +4,7 @@ import { XMarkIcon } from '@heroicons/react/24/outline'
 import { useEffect, useState } from 'react'
 import axios from 'axios'
 import logo from  "../assets/logo 3.png";
+import { Link } from 'react-router-dom'
 
 const navigation = [
   { name: 'Create Team', href: '/createteam'},
@@ -77,9 +78,9 @@ export function Navbar() {
               </div>
               <div className="hidden lg:flex lg:gap-x-12">
                 {navigation.map((item) => (
-                  <a key={item.name} href={item.href} className="text-sm font-semibold leading-6 text-gray-900">
+                  <Link key={item.name} to={item.href} className="text-sm font-semibold leading-6 text-gray-900">
                     {item.name}
-                  </a>
+                  </Link>
                 ))}
               </div>
               <div className="hidden lg:flex lg:flex-1 lg:justify-end">
